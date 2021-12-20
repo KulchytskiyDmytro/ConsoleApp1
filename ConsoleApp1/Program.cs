af9899eece5806109bfe821dc58f64477dfb090f
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2021-12-20 11:34:22 +0200

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int a1;
+int step;
+int n;
+int sum = 0;
+Console.WriteLine("Кульчицький Дмитро Олександрович");
+Console.WriteLine("Введите первый элемент");
+a1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите знаменатель прогрессии");
+step = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число элементов");
+n = Convert.ToInt32(Console.ReadLine());
+     
+sum = ((2*a1+step*(n-1))/2)*n;
+Console.WriteLine("Сумма членов прогрессии равна " + Convert.ToString(sum));
+Console.ReadKey();
